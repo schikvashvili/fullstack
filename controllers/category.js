ada709fd3e20701dd556875ada58c33c010ead43
talknshare.info@gmail.com <talknshare.info@gmail.com>
2019-01-23 17:11:54 +0200

    code update

--- controllers/category.js
+++ controllers/category.js
@@ -1,20 +1,56 @@
-module.exports.getAll = function(req, res){
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorHandlers');
+const Position = require('../models/Position');
 
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({ user: req.user.id });
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.getById = function(req, res){
-    
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.remove = function(req, res){
-
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({ _id: req.params.id })
+        await Position.remove({ category: req.params.id });
+        res.status(200).json({
+            message: 'The category has been removed'
+        })
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.create = function(req, res){
-    
+module.exports.create = async function (req, res) {
+    const category = new Category({
+        name: req.body.name,
+        imageSrc: req.file ? req.file.path : '',
+        user: req.user.id
+    });
+    try {
+        await category.save();
+        res.status(201).json(category);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
+module.exports.update = async function (req, res) {
+    try {
 
-module.exports.update = function(req, res){
-    
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const contoller = require('../controllers/category');
 const passport = require('passport');
+const upload = require('../middleware/upload');
+const contoller = require('../controllers/category');
 const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', {session: false}), contoller.getAll);
-router.get('/:id', contoller.getById);
-router.delete('/:id', contoller.remove);
-router.post('/', contoller.create);
-router.patch('/:id', contoller.update);
+router.get('/:id', passport.authenticate('jwt', {session: false}), contoller.getById);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), contoller.remove);
+router.post('/', passport.authenticate('jwt', {session: false}), upload.single('image'), contoller.create);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), upload.single('image'), contoller.update);
 
 module.exports = router;
